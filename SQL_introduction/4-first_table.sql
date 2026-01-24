ede916fa6059f5dc410e9423d1e6166b9638bbc5
isaacpadlim <isaac.padilla@anahuac.mx>
2026-01-24 00:29:20 -0600

    Task 4: create first_table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,6 @@
--- Creates a table called first_table
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- Creates the table first_table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
