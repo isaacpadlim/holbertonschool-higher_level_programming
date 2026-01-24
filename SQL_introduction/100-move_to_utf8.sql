27e2e24f5555469d96fb34fbcfd4919ca8b4c7bb
isaacpadlim <isaac.padilla@anahuac.mx>
2026-01-24 01:19:44 -0600

    Task 17: convert database and tables to UTF8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- Task 17: Converts hbtn_0c_0, first_table and its name field to UTF8 (utf8mb4_unicode_ci)
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
