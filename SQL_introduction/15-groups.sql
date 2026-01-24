a0c9d4dc24431219b70e04e18fd136e8c34bd37e
isaacpadlim <isaac.padilla@anahuac.mx>
2026-01-24 01:17:20 -0600

    Task 15: number of records by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Task 15: Lists the number of records for each score in second_table, ordered by count descending
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
