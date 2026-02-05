58c04893a50c1f620b7d6e384e9755af65e2b2a8
isaacpadlim <isaac.padilla@anahuac.mx>
2026-02-05 13:39:55 -0600

    Task #18: No Comedy tonight

---
+++ 101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- Uses a subquery to exclude shows linked to the genre Comedy
+-- Results are sorted in ascending order by the show title
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM tv_show_genres
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
+
