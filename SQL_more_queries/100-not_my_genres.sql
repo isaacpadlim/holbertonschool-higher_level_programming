e44ee6135a62b427977fb890e3217b7135a6462d
isaacpadlim <isaac.padilla@anahuac.mx>
2026-02-05 13:36:54 -0600

    task #17: Not my genre

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- Lists all genres not linked to the show Dexter.
+SELECT g.name
+FROM tv_genres AS g
+WHERE g.id NOT IN (
+    SELECT sg.genre_id
+    FROM tv_show_genres AS sg
+    INNER JOIN tv_shows AS s
+        ON sg.show_id = s.id
+    WHERE s.title = 'Dexter'
+)
+ORDER BY g.name ASC;
