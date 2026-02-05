1e1f89dd0a8f2c5b2d3d4ea7dee3bff40c10d1b9
isaacpadlim <isaac.padilla@anahuac.mx>
2026-02-05 13:41:51 -0600

    task 101: use title for exclusion

--- 101-not_a_comedy.sql
+++ 101-not_a_comedy.sql
@@ -1,13 +1,13 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
--- Uses a subquery to exclude shows linked to the genre Comedy
--- Results are sorted in ascending order by the show title
+-- The tv_genres table contains only one record where name = Comedy
+-- Results must be sorted in ascending order by the show title
 SELECT title
 FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
+WHERE title NOT IN (
+    SELECT tv_shows.title
+    FROM tv_shows
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY title ASC;
-
